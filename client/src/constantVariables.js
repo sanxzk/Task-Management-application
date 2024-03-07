4c704a8a28978a39eca28666421241bf6f0e99af
sanjana <sanjnasharma1411@gmail.com>
2024-03-07 09:25:51 +0530

    adding backend host

--- client/src/constantVariables.js
+++ client/src/constantVariables.js
@@ -1,2 +1,2 @@
-export const Host = 'http://localhost:5000'
-// export const Host = 'https://to-do-application-blush-ten.vercel.app'
+// export const Host = 'http://localhost:5000'
+export const Host = 'https://task-management-application-backend-nine.vercel.app/'
