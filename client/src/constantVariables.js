7e3a42e56fd82ac27a45e185c2260ce20bc9b828
sanjana <sanjnasharma1411@gmail.com>
2024-03-09 23:46:20 +0530

    resolving backend host error

--- client/src/constantVariables.js
+++ client/src/constantVariables.js
@@ -1,3 +1,3 @@
 // export const Host = 'http://localhost:5000'
-// export const Host = 'https://task-management-application-backend-nine.vercel.app/'
-export const Host =  "https://task-management-application-backend-sanxzk.vercel.app/";
+export const Host = 'https://task-management-application-backend-nine.vercel.app'
+// export const Host =  "https://task-management-application-backend-sanxzk.vercel.app";
